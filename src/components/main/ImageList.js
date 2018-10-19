faf1399f392558f02f8385ef99534b2a9865a77a
Jon Maldia <jon@maldia.net>
2018-10-18 21:50:58 -0400

    Add dummy data for images

--- src/components/main/ImageList.js
+++ src/components/main/ImageList.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Image from './Image'
 
 const ImageList = props => {
+    const sampleArray = [
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" },
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" },
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" },
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" },
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" },
+        { url: "https://imgplaceholder.com/320x320?text=Art+by+TRA" }
+    ]
     return (
         <div className="image-list">
             This is where the ImageList will be
--- src/components/sidebar/Social.js
+++ src/components/sidebar/Social.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Social = props => {
     return (
         <div className='social'>
-            <a href="facebook.com">
+            <a href="https://www.facebook.com" target="_blank">
                 <i class="fab fa-facebook"></i>
             </a>
-            <a href="instagram.com">
+            <a href="https://www.instagram.com/art.by.tra/" target="_blank">
                 <i class="fab fa-instagram"></i>
             </a>
-            <a href="twitter.com">
+            <a href="https://www.twitter.com" target="_blank">
                 <i class="fab fa-twitter-square"></i>
             </a>
         </div>

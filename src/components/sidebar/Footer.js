bc30e8130b213d4408470e8c88e0d532107aad49
Jon Maldia <jon@maldia.net>
2018-10-18 21:38:30 -0400

    Add social and copyright info on footer

---
+++ src/components/sidebar/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Social from './Social'
+import Copyright from './Copyright'
+
+const Footer = props => {
+    return (
+        <div className="footer" >
+            <Social />
+            <Copyright />
+        </div>
+    )
+}
+
+export default Footer

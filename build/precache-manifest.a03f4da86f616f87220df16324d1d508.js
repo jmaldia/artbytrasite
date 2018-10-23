8277cb6370c03f1476cc830d5597513dfc28fc0c
Jon Maldia <jon@maldia.net>
2018-10-23 17:13:48 -0400

    Add MailChimp Styles

---
+++ build/precache-manifest.a03f4da86f616f87220df16324d1d508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "588d3a7bd1fb69fc3253de743951fb72",
+    "url": "/static/media/logo.588d3a7b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "493d45f2542ede1e33ab",
+    "url": "/static/js/main.493d45f2.chunk.js"
+  },
+  {
+    "revision": "46e04b11e479a72b6f8e",
+    "url": "/static/js/1.46e04b11.chunk.js"
+  },
+  {
+    "revision": "493d45f2542ede1e33ab",
+    "url": "/static/css/main.f5cdfc91.chunk.css"
+  },
+  {
+    "revision": "75d3fae3aa13fabad40d290ec7e8bd4c",
+    "url": "/index.html"
+  }
+];

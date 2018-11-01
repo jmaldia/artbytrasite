5f980937a6668c441bce033e219618cb661d8f1b
Jon Maldia <jon@maldia.net>
2018-10-31 20:39:29 -0400

    Style: code for responsiveness

--- src/components/main/ImageList.js
+++ src/components/main/ImageList.js
@@ -4,21 +4,22 @@ import Image from './Image'
 const ImageList = props => {
     const images = [
         { url: "http://jonmaldia.com/artbytra/art-01.jpg" },
-        { url: "http://jonmaldia.com/artbytra/art-02.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-03.jpg" },
-        { url: "http://jonmaldia.com/artbytra/art-04.jpg" },
-        // { url: "http://jonmaldia.com/artbytra/art-05.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-06.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-07.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-08.jpg" },
-        // { url: "http://jonmaldia.com/artbytra/art-09.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-10.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-11.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-12.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-13.jpg" },
         { url: "http://jonmaldia.com/artbytra/art-14.jpg" },
-        // { url: "http://jonmaldia.com/artbytra/art-15.jpg" },
+        { url: "http://i58.photobucket.com/albums/g258/artbytra/art-15_zpssnzjbtzf.jpg" },
+        // { url: "http://jonmaldia.com/artbytra/art-04.jpg" },
+        // { url: "http://jonmaldia.com/artbytra/art-05.jpg" },
+        // { url: "http://jonmaldia.com/artbytra/art-09.jpg" },
+        // { url: "http://jonmaldia.com/artbytra/art-15.jpg" }
         // { url: "http://jonmaldia.com/artbytra/art-16.jpg" }
+        // { url: "http://jonmaldia.com/artbytra/art-02.jpg" },
     ]
 
     return (

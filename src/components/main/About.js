fe41915da28af7a059aed2df21a1019a2f4dc9dc
Jon Maldia <jon@maldia.net>
2019-01-08 22:30:59 -0500

    Changes to styles for responsiveness

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -4,28 +4,28 @@ import Contact from './Contact'
 const About = props => {
     return (
         <div className="info">
-        <div className="about" >
-            <p className="about-p">
-                Hello! Todd R. Ackerman hails, proudly, from one of the most creative cities he knows... Toledo, Ohio. Home to the Mud Hens, Walleye hockey, and of course Marcos Pizza. Toledo artistically, is one of the most happening places!
-                He currently resides in Queens, NY where you might find him at various Green Bay Packers bars, or single-handedly trying to revive the Detroit Red Wings playoff appearance streak, octopus in one hand, drawing pens and paper in the other. 
-            </p>
-            <p className="about-p">
-                ​Aside from what you see here, he has a wealth of abstract and digital illustrations, and is a true appreciator of ALL art mediums.
-                Todd is constantly crafting his visual story telling skills, for a life as a professional children's/picture/educational book illustrator. Check back often for new art! 
-                Todd is currently seeking representation. 
-            </p>
-            <p className="about-p">
-                Thank you so much for visiting.
-            </p>
-            <p className="about-p">
-                A rare photo of two Todds...
-            </p>
-            <img  className="about-p" src="http://www.fauxtoddgraphy.com/uploads/1/0/5/9/10598479/published/file-jul-27-9-45-42-pm.jpeg?1501432664" alt="The Tale of Two Todds"/>
+            <div className="about" >
+                <p className="about-p">
+                    Hello! Todd R. Ackerman hails, proudly, from one of the most creative cities he knows... Toledo, Ohio. Home to the Mud Hens, Walleye hockey, and of course Marcos Pizza. Toledo artistically, is one of the most happening places!
+                    He currently resides in Queens, NY where you might find him at various Green Bay Packers bars, or single-handedly trying to revive the Detroit Red Wings playoff appearance streak, octopus in one hand, drawing pens and paper in the other. 
+                </p>
+                <p className="about-p">
+                    ​Aside from what you see here, he has a wealth of abstract and digital illustrations, and is a true appreciator of ALL art mediums.
+                    Todd is constantly crafting his visual story telling skills, for a life as a professional children's/picture/educational book illustrator. Check back often for new art! 
+                    Todd is currently seeking representation. 
+                </p>
+                <p className="about-p">
+                    Thank you so much for visiting.
+                </p>
+                <p className="about-p">
+                    A rare photo of two Todds...
+                </p>
+                <img  className="about-p" src="http://www.fauxtoddgraphy.com/uploads/1/0/5/9/10598479/published/file-jul-27-9-45-42-pm.jpeg?1501432664" alt="The Tale of Two Todds"/>
 
-            <p className="about-p">Check back often for new art! & feel free to email me directly at <a href="mailto:todda99@outlook.com">todda99@outlook.com</a> or scroll below to leave a message.</p>
-            <p className="about-p">Todd is currently seeking representation.</p>
-        </div>
-        <Contact />
+                <p className="about-p">Check back often for new art! & feel free to email me directly at <a href="mailto:todda99@outlook.com">todda99@outlook.com</a> or scroll below to leave a message.</p>
+                <p className="about-p">Todd is currently seeking representation.</p>
+            </div>
+            <Contact />
         </div>
     )
 }

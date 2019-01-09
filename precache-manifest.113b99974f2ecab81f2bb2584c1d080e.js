5188579d5cc77f5ac870c04a2bca0bfe395a4e44
Jon Maldia <jon@maldia.net>
2019-01-08 22:48:25 -0500

    Updates

---
+++ precache-manifest.113b99974f2ecab81f2bb2584c1d080e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "588d3a7bd1fb69fc3253de743951fb72",
+    "url": "/artbytra/static/media/logo.588d3a7b.jpg"
+  },
+  {
+    "revision": "c39a9720907b8d7fdaee",
+    "url": "/artbytra/static/js/runtime~main.c39a9720.js"
+  },
+  {
+    "revision": "3b19c5a2ebd7ae76c1c9",
+    "url": "/artbytra/static/js/main.3b19c5a2.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/artbytra/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "3b19c5a2ebd7ae76c1c9",
+    "url": "/artbytra/static/css/main.43a5e2a8.chunk.css"
+  },
+  {
+    "revision": "d21bca0701ef4f5e1102468abb5b884a",
+    "url": "/artbytra/index.html"
+  }
+];
